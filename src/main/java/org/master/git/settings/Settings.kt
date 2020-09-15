3335f0cdcf3c45c46563e838deec26dadfed3b29
deepan <deepan.elango@spectrum.global>
2020-09-15 19:47:09 +0530

    Added move up, down actions and template text field

--- src/main/java/org/master/git/settings/Settings.kt
+++ src/main/java/org/master/git/settings/Settings.kt
@@ -2,8 +2,11 @@ package org.master.git.settings
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.intellij.icons.AllIcons
 import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.options.Configurable
+import com.intellij.ui.AnActionButton
 import com.intellij.ui.ToolbarDecorator
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.layout.panel
@@ -12,9 +15,7 @@ import org.master.git.AddItemDialog
 import org.master.git.ItemModel
 import java.awt.BorderLayout
 import java.util.*
-import javax.swing.JComponent
-import javax.swing.JPanel
-import javax.swing.ListSelectionModel
+import javax.swing.*
 import javax.swing.event.DocumentEvent
 import javax.swing.event.DocumentListener
 import javax.swing.event.ListSelectionListener
@@ -26,6 +27,7 @@ import kotlin.collections.ArrayList
 class Settings : Configurable, DocumentListener {
 
     private val configTable = JBTable()
+    private val templateArea = JTextField()
 
     var dtm = object : DefaultTableModel() {
         override fun isCellEditable(row: Int, column: Int): Boolean = false
@@ -35,10 +37,17 @@ class Settings : Configurable, DocumentListener {
 
     var column = arrayOf("NAME", "TYPE", "VARIABLE", "VALUE")
 
+    private var cachedItems: ArrayList<ItemModel> = ArrayList()
+
     override fun createComponent(): JComponent? {
         val config = PropertiesComponent.getInstance()
         val itemsJson = config.getValue("ITEMS_JSON", Gson().toJson(ArrayList<ItemModel>()).toString())
-        val items: ArrayList<ItemModel> = Gson().fromJson(itemsJson, object : TypeToken<ArrayList<ItemModel>>() {}.type)
+        val template = config.getValue("TEMPLATE", "")
+        cachedItems = try {
+            Gson().fromJson(itemsJson, object : TypeToken<ArrayList<ItemModel>>() {}.type)
+        } catch (e: Exception) {
+            ArrayList()
+        }
 
         val scroll = JBScrollPane()
 
@@ -58,26 +67,55 @@ class Settings : Configurable, DocumentListener {
             if (event.valueIsAdjusting) return@ListSelectionListener
         })
 
+        val refreshAction = object : AnActionButton("Refresh", AllIcons.Actions.Refresh) {
+            override fun actionPerformed(anActionEvent: AnActionEvent) {
+                populateCachedValues()
+            }
+        }
+
         val tableToolbarDecorator: ToolbarDecorator = ToolbarDecorator.createDecorator(configTable).setAddAction {
             AddItemDialog(getCurrentItems(), this::addRow).show()
         }.setRemoveAction {
             dtm.removeRow(configTable.selectedRow)
-        }.setRemoveActionUpdater { configTable.selectedRow != -1 }.disableUpDownActions()
+        }.setRemoveActionUpdater { configTable.selectedRow != -1 }
+            .setMoveUpAction {
+                val row = configTable.selectedRow
+                dtm.moveRow(row, row, row - 1)
+                configTable.setRowSelectionInterval(row - 1, row - 1)
+            }.setMoveDownAction {
+                val row = configTable.selectedRow
+                dtm.moveRow(row, row, row + 1)
+                configTable.setRowSelectionInterval(row + 1, row + 1)
+            }.addExtraAction(refreshAction)
 
         val panelTable = tableToolbarDecorator.createPanel()
 
         scroll.add(panelTable)
 
-        if (items.isNotEmpty()) {
-            for (i in 0 until items.size) {
-                createValueRow(items[i])
-            }
-        }
+        populateCachedValues()
 
+        templateArea.text = template
         return panel {
             noteRow("Customize the list of templates to be included in the Git Master View")
             row {
                 panelTable(grow)
+            }
+            row {
+                noteRow("Write your own template by using the variable names created above enclosed within \${variableName}\nEg: [\${type}] - [\${issueName}] [\${environment}]: \${commitMessage}")
+            }
+            row {
+                templateArea(grow)
+            }
+        }
+    }
+
+    private fun populateCachedValues() {
+        while (dtm.rowCount > 0) {
+            dtm.removeRow(0)
+        }
+        if (cachedItems.isNotEmpty()) {
+            for (i in 0 until cachedItems.size) {
+                createValueRow(cachedItems[i])
             }
         }
     }
@@ -109,6 +147,7 @@ class Settings : Configurable, DocumentListener {
     override fun apply() {
         val config = PropertiesComponent.getInstance()
         config.setValue("ITEMS_JSON", Gson().toJson(getCurrentItems()).toString())
+        config.setValue("TEMPLATE", templateArea.text)
         modified = false
     }
 
